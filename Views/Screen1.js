16107137dcbf2c7d48525101212146aa2ffc9647
saifulislam05 <saiful.bijulia@gmail.com>
2023-02-02 22:11:55 +0530

    error handle and works for screen 2

--- Views/Screen1.js
+++ Views/Screen1.js
@@ -1,37 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { Text,View,StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native'
-import {useRecoilState} from 'recoil'
-import { inputtextwallpaper } from '../atoms/wallpaperInputText';
-import Navbar from './Navbar';
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useRecoilState } from "recoil";
+import { inputtextwallpaper } from "../atoms/wallpaperInputText";
+import Navbar from "./Navbar";
 
-const Screen1 = ({navigation}) => {
+const Screen1 = ({ navigation }) => {
   const access_key = "B3HB7ZSVgJuYTI5B8V1u-EO1DhL1XPdvg4Wrtrdqyac";
   const [searchValue, setSearchValue] = useRecoilState(inputtextwallpaper);
-  const [imageCollection,setImageCollection]=useState([])
-  
+  const [imageCollection, setImageCollection] = useState([]);
+
   // console.log(searchValue);
 
   useEffect(() => {
     const getImageCollection = async () => {
-    let data = await fetch(
-      `https://api.unsplash.com/search/collections?page=1&per_page=40&query=${searchValue}&client_id=${access_key}`
-      ); 
-      let jsonData = await data.json()
-      setImageCollection(jsonData)
+      let data = await fetch(
+        `https://api.unsplash.com/search/collections?page=1&per_page=40&query=${searchValue}&client_id=${access_key}`
+      );
+      let jsonData = await data.json();
+      setImageCollection(jsonData);
       console.log(jsonData.results[0].cover_photo.urls.regular);
-    }
+    };
     getImageCollection();
   }, [searchValue]);
-  
 
-imageCollection.total==0 && setSearchValue('all')
+  imageCollection.total == 0 && setSearchValue("all");
 
   const ShowWallpaper = (item) => {
-    navigation.navigate('screen2',{clickedImage:`${JSON.stringify(item)}`})
-  }
+    navigation.navigate("screen2", { clickedImage: `${JSON.stringify(item)}` });
+  };
   return (
     <View style={styles.container}>
-      <Navbar/>
+      <Navbar />
       <Text>{searchValue} related images</Text>
       <FlatList
         numColumns={2}
@@ -53,22 +59,21 @@ imageCollection.total==0 && setSearchValue('all')
 
 const styles = StyleSheet.create({
   container: {
-        width: "100%",
-      height:'100%',
+    width: "100%",
+    height: "100%",
     backgroundColor: 20,
     paddingVertical: 10,
-        alignItems: "center",
+    alignItems: "center",
   },
   imageContainer: {
     width: 200,
     height: 300,
     backgroundColor: "white",
     padding: 8,
-
   },
   image: {
-    width: '100%',
-    height: '100%',
-  }
+    width: "100%",
+    height: "100%",
+  },
 });
-export default Screen1
+export default Screen1;
--- Views/Screen2.js
+++ Views/Screen2.js
@@ -1,10 +1,106 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useEffect, useState } from "react";
+import {
+  PermissionsAndroid,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
-const Screen2 = () => {
+
+const Screen2 = ({ route }) => {
+  const { clickedImage } = route.params;
+  const [imageData, setImageData] = useState();
+
+  useEffect(() => {
+    setImageData(JSON.parse(clickedImage));
+  }, []);
+
+  // const downloadImage = async () => {
+  //   try {
+  //     const granted = await PermissionsAndroid.request(
+  //       PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+  //     );
+  //     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+  //       const { uri } = imageData.cover_photo.urls;
+  //       const downloadDest = `${RNFS.ExternalStorageDirectoryPath}/${uri
+  //         .split("/")
+  //         .pop()}`;
+  //       const ret = await RNFS.downloadFile({
+  //         fromUrl: uri,
+  //         toFile: downloadDest,
+  //       });
+  //       if (ret.statusCode === 200) {
+  //         console.log("Image download success");
+  //       }
+  //     } else {
+  //       console.log("Permission denied");
+  //     }
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
   return (
-    <Text>screen2</Text>
-  )
-}
+    <View style={styles.imageContainer}>
+      {typeof imageData == "object" && (
+        <Image
+          source={{ uri: imageData.cover_photo.urls.regular }}
+          style={styles.image}
+        />
+      )}
 
-export default Screen2
+      <TouchableOpacity style={styles.downloadBtn} >
+        <Text style={styles.donwloadBtnText}>Download</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.nextBtn}>
+        <Text style={styles.nextBtnText}>Next</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  downloadBtn: {
+    position: "absolute",
+    left: 60,
+    bottom: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "#000",
+    elevation: 20,
+    borderRadius: 20,
+  },
+  donwloadBtnText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "900",
+  },
+  nextBtn: {
+    position: "absolute",
+    right: 60,
+    bottom: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "#000",
+    elevation: 20,
+    borderRadius: 20,
+  },
+  nextBtnText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "900",
+  },
+});
+
+export default Screen2;
